bce9683c8e97ddcf697c773d31ebd23c709d7c48
daniyous <daniyousking@gmail.com>
2023-03-21 10:29:29 -0700

    alpahbet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alhabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for (char i="a"; i<="z"; i++)
+	       _putchar('i');
+	_putchar('\n');
+
+	return (0);
+}
+
