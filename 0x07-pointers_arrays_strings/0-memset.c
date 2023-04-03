2f6454d68813be53049ef9e4a9dcacfa140134ce
daniyous <daniyousking@gmail.com>
2023-04-03 11:25:52 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _memest- shis is a pointer to the block of memory to fill.
+ * @s: his is a pointer to the block of memory to fill.
+ * @b: This is the value to be set.
+ * @n: the size of numbers
+ *
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n){
+
