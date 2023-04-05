0f33a4805568ad8f65c831e1360a514f8e503b33
daniyous <daniyousking@gmail.com>
2023-04-05 03:43:26 -0700

    function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,15 @@
  * Return: Always 0 (success)
  */
 
-char *_memset(char *s, char b, unsigned int n){
-        unsigned int i;
-        
-        for (i = 0; i < n; i++)
-                s[i] = b;
-        return (s);
-} 
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
 
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
@@ -13,5 +11,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 #endif
+
